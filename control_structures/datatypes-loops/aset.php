75b04d8be6faa160da0db856e96735fe45e9d799
bhavana05 <bhavana.yellumahanthi@gmail.com>
2017-06-28 11:03:22 +0530

    updated program

--- control_structures/datatypes-loops/aset.php
+++ control_structures/datatypes-loops/aset.php
@@ -1,8 +1,18 @@
 <?php
 
-echo "enter list of your choice with repetition:";
-$str = trim(fgets(STDIN, 1024));
-$input=explode(" ",$str);
-$result = array_unique($input);
-print_r($result);
+//the message that is to be displayed is stored in msg variable
+$msg = "enter list of your choice with repetition:";
+//echo displays the message
+echo $msg;
+//STDIN takes the input from the user
+$a = fgets(STDIN, 1024);
+//trin function avoids the whitespaces from the user input
+$a = trim($a);
+//explode function splits the numbers in to array
+$b = explode(" ",$a);
+//array_unique function takes the unique elements from the array
+$c = array_unique($b);
+//print_r functions prints the entire array
+print_r($c);
+
 ?>
